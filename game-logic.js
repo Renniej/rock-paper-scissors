0ddfad666d3e1eab484e52d4543baa414ca60416
Rennie <7017622+Renniej@users.noreply.github.com>
2025-03-24 04:50:13 -0400

    add rock, paper ,scissor enums

--- game-logic.js
+++ game-logic.js
@@ -1,16 +1,32 @@
 
 console.log("Hello world")
 console.log(getComputerChoice())
+
+const Choice = Object.freeze({
+    SCISSORS : Symbol("Scissors"),
+    PAPER : Symbol("Paper"),
+    ROCK : Symbol("Rock"),
+    INVALID : Symbol("Invalid")
+})
+
+
+function getHumanChoice() {
+    const playerChoice = prompt("Choose between rock, paper, or scissors").toLocaleLowerCase().trim()
+
+
+}
+
+
 function getComputerChoice() {
-    let choice = ""
+    let choice = Choice.INVALID
     const randomNumber = Math.random()
 
     if (randomNumber <= 0.33) {
-        choice = "rock"
+        choice = Choice.PAPER
     } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
-        choice = "paper"
+        choice = Choice.ROCK
     } else {
-        choice = "scissors"
+        choice = Choice.SCISSORS
     }
 
     return choice
